2913a2044e593421b7e8f7297920148cd590efb0
GZZ6 <146560910+GZZ6@users.noreply.github.com>
2024-02-13 18:39:24 -0500

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.iidk.gorillatag.GZZ6";
+        public const string Name = "GZZ6";
+        public const string Description = "You are using version 1";
+        public const string Version = "1.0.0";
+    }
+}
